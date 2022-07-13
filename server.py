90391766ae36e1a6f1de34eafbf884bca54630ea
mayosen <gare577@gmail.com>
2022-07-14 01:17:20 +0300

    Add code snippet for aiohttp server

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 import asyncio
 
 import aiofiles
+import aiohttp
+from aiohttp import web
+from aiohttp.web_request import Request
 
 
 async def make_archive(folder: str = "./data", name: str = "photos"):
@@ -23,4 +26,20 @@ async def main():
     await make_archive(name="dd")
 
 
-asyncio.run(main())
+async def archive(request: Request):
+    raise NotImplementedError
+
+
+async def handle_index_page(request: Request):
+    async with aiofiles.open("index.html", "r") as index_file:
+        index_contents = await index_file.read()
+    return web.Response(text=index_contents, content_type="text/html")
+
+
+if __name__ == "__main__":
+    app = web.Application()
+    app.add_routes([
+        web.get("/", handle_index_page),
+        web.get("/archive/{archive_hash}", archive)
+    ])
+    web.run_app(app)
